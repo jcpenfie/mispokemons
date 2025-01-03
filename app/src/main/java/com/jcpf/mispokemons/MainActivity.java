e930daa30726df1339b6c6a5a392eea8d87390af
jcpenfie <juancarlospfierro@gmail.com>
2025-01-03 20:59:42 +0100

    🧭 Navegación 🌈 Configuración inicial del thema

--- app/src/main/java/com/jcpf/mispokemons/MainActivity.java
+++ app/src/main/java/com/jcpf/mispokemons/MainActivity.java
@@ -1,24 +1,59 @@
 package com.jcpf.mispokemons;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.jcpf.mispokemons.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        Fragment navHostFragment = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        if(navHostFragment != null){
+            navController = NavHostFragment.findNavController(navHostFragment);
+            NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+        }
+
+        replaceFragment(new MisPokemonsFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.mis_pokemons_menu) {
+                replaceFragment(new MisPokemonsFragment());
+                return true;
+            } else if (item.getItemId() == R.id.pokedex_menu) {
+                replaceFragment(new PokedexFragment());
+                return true;
+            } else if (item.getItemId() == R.id.ajustes_menu) {
+                replaceFragment(new AjustesFragment());
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
         });
+        setContentView(binding.getRoot());
     }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.nav_host_fragment, fragment);
+        fragmentTransaction.commit();
+    }
+
+
 }
