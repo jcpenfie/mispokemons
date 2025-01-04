42f46cbd0a940856a48f30e30af4709b5a5d1e77
jcpenfie <juancarlospfierro@gmail.com>
2025-01-04 11:18:56 +0100

    ⚙️Primera versión de los ajustes

--- app/src/main/java/com/jcpf/mispokemons/AjustesFragment.java
+++ app/src/main/java/com/jcpf/mispokemons/AjustesFragment.java
@@ -1,64 +1,106 @@
 package com.jcpf.mispokemons;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AjustesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.navigation.NavigationView;
+import com.jcpf.mispokemons.databinding.FragmentAjustesBinding;
+import com.jcpf.mispokemons.databinding.FragmentMisPokemonsBinding;
+import com.jcpf.mispokemons.databinding.SwitchItemBinding;
+
+import java.util.Objects;
+
+
 public class AjustesFragment extends Fragment {
+    
+    FragmentAjustesBinding binding;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AjustesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AjustesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AjustesFragment newInstance(String param1, String param2) {
-        AjustesFragment fragment = new AjustesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentAjustesBinding.inflate(inflater, container, false);
+
+        //Idioma
+        binding.ajustesView.getMenu().getItem(1).setOnMenuItemClickListener(item -> cambiarIdiomas());
+
+
+        // todo: eliminar mis pokemons
+
+
+        //Acerca de...
+        binding.ajustesView.getMenu().getItem(3).setOnMenuItemClickListener(item -> infoClicked());
+
+        //Cerrar sesión
+        binding.ajustesView.getMenu().getItem(4).setOnMenuItemClickListener(item -> cerrarSesion());
+
+
+        return binding.getRoot();
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ajustes, container, false);
+
+
+    private boolean cambiarIdiomas() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+        builder.setTitle(R.string.idioma)
+                .setSingleChoiceItems(R.array.idiomas_array, 0,(dialog, which) -> {
+                    // Obtengo el array de idiomas desde los recursos
+                    String[] idiomas = getResources().getStringArray(R.array.idiomas_array);
+
+                    // TODO: cambio de idioma
+                    // Mostrar un mensaje con el idioma seleccionado
+                    Toast.makeText(getContext(), R.string.cambiar_message, Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        return true;
     }
+
+    private boolean cerrarSesion() {
+        // 1. Instanciamos el AlertDialog con su constructor.
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        // 2. Añdimos lo que va a mostrar.
+        builder.setMessage(R.string.cerrar_message).setTitle(R.string.cerrar_sesi_n)
+                .setPositiveButton(R.string.aceptar, (dialog, id) -> {
+                    //todo: cerrar sesión
+                    Toast.makeText(getContext(), "Se ha cerrado la sesión", Toast.LENGTH_SHORT).show();
+                })
+                .setNegativeButton(R.string.cancelar, (dialog, id) -> {
+                });;
+        // 3. Creamos el AlertDialog.
+        AlertDialog dialog = builder.create();
+        //4. Lo mostramos
+        dialog.show();
+        return true;
+    }
+
+    private boolean infoClicked() {
+        // 1. Instanciamos el AlertDialog con su constructor.
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        // 2. Añdimos lo que va a mostrar.
+        builder.setMessage(R.string.dialog_message).setTitle(R.string.acerca_de);
+        // 3. Creamos el AlertDialog.
+        AlertDialog dialog = builder.create();
+        //4. Lo mostramos
+        dialog.show();
+        return true;
+    }
+
+
 }
